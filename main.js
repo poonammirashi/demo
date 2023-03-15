4a4941d29065446709339270857ba26e5e219dcb
poonammirashi <poonam200023@gmail.com>
2023-03-15 04:53:29 -0700

    as task 12

--- main.js
+++ main.js
@@ -1,19 +1,28 @@
-localStorage.setItem('userdetailsemailid','name,age');
-var button=document.querySelector('.btn');
+localStorage.setItem('userdetailsemailid', 'name,email,number');
+var button = document.querySelector('.btn');
 console.log(localStorage.getItem('userdetailsemailid'));
 
-button.addEventListener('click',addevent);
+button.addEventListener('click', addevent);
 
-function addevent(e) { 
+function addevent(e) {
     e.preventDefault();
-    var email=document.getElementById('email').value;
-    var name=document.querySelector('#name').value;
+    var email = document.getElementById('email').value;
+    var name = document.querySelector('#name').value;
+    var num = document.querySelector('#number').value;
     var obj = {
-        person:name,
-        age:23
+        person_is: name,
+        email_is: email,
+        number_is: num
     }
     var obj_serialized = JSON.stringify(obj);
     localStorage.setItem(email, obj_serialized);
-    email='';
-    name='';
+
+    var itemlist = document.querySelector('.items')
+    var li = document.createElement('li');
+
+    li.className = 'item';
+    li.appendChild(document.createTextNode(name + '-' + email +'-' + (num)));
+    itemlist.appendChild(li)
+    email = '';
+    name = '';
 }
